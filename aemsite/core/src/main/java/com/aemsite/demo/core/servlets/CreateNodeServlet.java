c69499b1d710cdb8563beb1697c9c653f0bb3148
nisangit <nishanbagavathi@gmail.com>
2023-10-05 11:09:46 +0530

    create node programmatically

---
+++ aemsite/core/src/main/java/com/aemsite/demo/core/servlets/CreateNodeServlet.java
@@ -0,0 +1,54 @@
+package com.aemsite.demo.core.servlets;
+import java.io.IOException;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@Component(
+        service = Servlet.class,
+        property = {
+                Constants.SERVICE_DESCRIPTION + "=Create Node Servlet",
+                "sling.servlet.methods=POST",
+                "sling.servlet.paths=/bin/createNode"
+        }
+)
+public class CreateNodeServlet extends SlingAllMethodsServlet {
+    private static final long serialVersionUID = 1L;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        ResourceResolver resourceResolver = request.getResourceResolver();
+        Session session = resourceResolver.adaptTo(Session.class);
+        try {
+            // Define the path where you want to create the node
+            String nodePath = "/content/aemsite/newNode";
+            // Check if the node doesn't exist already
+            if (!session.nodeExists(nodePath))
+            {
+                Node parentNode = session.getNode("/content/aemsite");
+                Node newNode = parentNode.addNode("newNode","nt:unstructured");
+                newNode.setProperty("propertyName", "propertyValue");
+                session.save();
+                response.getWriter().write("Node created successfully.");
+            } else {
+                response.getWriter().write("Node already exists.");
+            }
+        } catch (RepositoryException e) {
+            logger.error("Error creating node: " + e.getMessage(), e);
+            response.getWriter().write("Error: " + e.getMessage());
+        } finally {
+            if (session != null && session.isLive()) {
+                session.logout();
+            }
+        }
+    }
+}
